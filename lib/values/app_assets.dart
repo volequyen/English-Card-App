535ee62a4a758fdd633d1471395e2e7ce6a3893e
volequyen <qvo9110@gmail.com>
2025-04-14 23:48:28 +0700

    page divided

---
+++ lib/values/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static final String imagePath = 'assets/image';
+  static final String exchange = imagePath + 'exchange.svg';
+  static final String  leftArrow = imagePath + 'left-arrow.png';
+  static final String rightArrow = imagePath + 'right-arrow.png';
+  static final String menu = imagePath + 'menu.svg';
+
+}
---
+++ lib/values/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xffa5d6a7);
+  static const blackGray = Color(777777);
+  static const secondColor = Color(0xff81C784);
+  static const textColor = Color(0xff211500);
+  static const lightGreen = Color(0xff66bb6a);
+  static const greyText = Color(0xff898788);
+  static const lightGray = Color(0xffb2b68d);
+}
