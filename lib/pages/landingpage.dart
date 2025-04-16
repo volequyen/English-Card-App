4c3354d0dfe121dbbd91961ad65e2e9cab3b4dce
volequyen <qvo9110@gmail.com>
2025-04-16 23:58:01 +0700

    welcome

---
+++ lib/pages/landingpage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Landingpage extends StatelessWidget {
+  const Landingpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
