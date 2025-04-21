260123a06056a3489691fcd0c2ad4133f741bddb
volequyen <your-email@example.com>
2025-04-21 23:58:02 +0700

    bo cuc

--- lib/pages/landingpage.dart
+++ lib/pages/landingpage.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Landingpage extends StatelessWidget {
   const Landingpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+    
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(color: Colors.red),
+          ),
+          Expanded(
+            child: Container(color: Colors.yellow),
+          ),
+          Expanded(
+            child: Container(color: Colors.green),
+          ),
+        ],
+      ),
+    );
   }
 }
